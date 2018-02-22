b99778eea366c9cd7994cd06db1af86a62cee6e3
Srinivaik104 <vaikunth@kth.se>
2018-02-22 11:22:25 +0100

    Update rb

--- server/src/main/scala/se/kth/id2203/broadcast/rb/ReliableBroadcast.scala
+++ server/src/main/scala/se/kth/id2203/broadcast/rb/ReliableBroadcast.scala
@@ -1,24 +1,34 @@
 package se.kth.id2203.broadcast.rb
 
+import se.kth.id2203.broadcast.beb.{BestEffortBroadcastDeliver, BestEffortBroadcastPort}
 import se.sics.kompics.sl.{ComponentDefinition, NegativePort, PositivePort, handle}
 import se.sics.kompics.network.Network
 import se.kth.id2203.networking.{NetAddress, NetMessage}
-
+import se.kth.id2203.broadcast.rb.ReliableBroadcastMessage
 
 class ReliableBroadcast extends ComponentDefinition{
+  private val bestEffortBroadcast: PositivePort[BestEffortBroadcastPort] = requires[BestEffortBroadcastPort]
   private val reliableBroadcast: NegativePort[ReliableBroadcastPort] = provides[ReliableBroadcastPort]
-  private val net: PositivePort[Network] = requires[Network]
   private val self = config.getValue("id2203.project.address", classOf[NetAddress])
+  private var seqNum = 0;
+  private val delivered = collection.mutable.Set[ReliableBroadcastMessage]()
+
 
   reliableBroadcast uponEvent {
     case request: ReliableBroadcastRequest => handle {
-      request.addresses.foreach(address => trigger(NetMessage(self, address, ReliableBroadcastMessage(self, request.event)),net))
+      seqNum += 1
+      val msg = ReliableBroadcastMessage(self, ReliableBroadcastDeliver(self, request.event), seqNum)
+      trigger(bestEffortBroadcast(msg))
     }
   }
 
-  net uponEvent {
-    case NetMessage(src, payload: ReliableBroadcastMessage) => handle {
-      trigger(ReliableBroadcastDeliver(payload.src, payload.event), reliableBroadcast)
+  bestEffortBroadcast uponEvent {
+    case BestEffortBroadcastDeliver(src, payload: ReliableBroadcastMessage) => handle {
+      if(!delivered.contains(payload)){
+        delivered.add(payload)
+        trigger(ReliableBroadcastDeliver(payload.src, payload.data), reliableBroadcast)
+        trigger(bestEffortBroadcast(payload))
+      }
     }
   }
 }
